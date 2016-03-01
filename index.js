720dc6dd4becea522a610ea30adb84ec1c85b516
arunthampi <arun.thampi@gmail.com>
2016-02-29 17:10:15 -0800

    Complete github-committers

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var path = require('path');
 module.exports = function(robot) {
   robot.loadFile(path.resolve(__dirname, "scripts"), "github-activity.js");
   robot.loadFile(path.resolve(__dirname, "scripts"), "github-commit-link.js");
+  robot.loadFile(path.resolve(__dirname, "scripts"), "github-committers.js");
 };
---
+++ scripts/github-committers.js
@@ -0,0 +1,49 @@
+module.exports = function(robot) {
+  var github = require("githubot")(robot);
+
+  var read_contributors = function(msg, done, response_handler) {
+    var repo = github.qualified_repo(msg.match[1]);
+    var base_url = process.env.HUBOT_GITHUB_API || 'https://api.github.com';
+    var url = base_url + "/repos/" + repo + "/contributors";
+
+    github.get(url, function(commits) {
+      if (commits.message) {
+        msg.send("Achievement unlocked: [NEEDLE IN A HAYSTACK] repository " + commits.message + "!", done);
+      } else if (commits.length === 0) {
+        msg.send("Achievement unlocked: [LIKE A BOSS] no commits found!", done);
+      } else {
+        response_handler(commits);
+      }
+    });
+  };
+
+  robot.respond(/repo committers (.*)$/i, function(msg, done) {
+    read_contributors(msg, done, function(commits) {
+      var max_length = 20;
+      var results = [];
+
+      for (var i = 0; i < Math.min(commits.length, max_length); i++) {
+        commit = commits[i];
+        results.push("[" + commit.login + "] Contributions: " + commit.contributions);
+      }
+
+      msg.send(results, done);
+    });
+  });
+
+  robot.respond(/repo top-committers? (.*)$/i, function(msg, done) {
+    read_contributors(msg, done, function(commits) {
+      var top_commiter = null;
+      for (var i = 0; i < commits.length; i++) {
+        var commit = commits[i];
+        if (top_commiter === null) {
+          top_commiter = commit;
+        }
+        if (commit.contributions > top_commiter.contributions) {
+          top_commiter = commit;
+        }
+      }
+      msg.send("[" + top_commiter.login + "] " + top_commiter.contributions + " :trophy:", done);
+    });
+  });
+};
