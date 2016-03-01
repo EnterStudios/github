04d76262f9a62d40a8fe8a52f9dd31a7c27dd7f9
arunthampi <arun.thampi@gmail.com>
2016-02-29 18:06:29 -0800

    Add github-search, github-pulls and github-merge

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ module.exports = function(robot) {
   robot.loadFile(path.resolve(__dirname, "scripts"), "github-activity.js");
   robot.loadFile(path.resolve(__dirname, "scripts"), "github-committers.js");
   robot.loadFile(path.resolve(__dirname, "scripts"), "github-issues.js");
+  robot.loadFile(path.resolve(__dirname, "scripts"), "github-merge.js");
+  robot.loadFile(path.resolve(__dirname, "scripts"), "github-pulls.js");
+  robot.loadFile(path.resolve(__dirname, "scripts"), "github-search.js");
 };
